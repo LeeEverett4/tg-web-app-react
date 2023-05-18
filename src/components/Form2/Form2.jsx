1f0500db87bc406d5b5bdcb741495aa6f5b776a8
leeeverett4 <kazakoff.iurij2018@gmail.com>
2023-05-18 20:22:47 +0300

    init

---
+++ src/components/Form2/Form2.jsx
@@ -0,0 +1,92 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
+import {useTelegram} from "../../Hooks/useTelegram";
+
+const Form = () => {
+    const [name, setCountry] = useState('');
+    const [repos, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const [mail, setMail] = useState('');
+    const {tg} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            name,
+            repos,
+            mail,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [name, repos, mail,subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!name || !repos) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [name, repos])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+    const onChangemail = (e) => {
+        setMail(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+                value={name}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Отзыв'}
+                value={repos}
+                onChange={onChangeStreet}
+            />
+            <input
+
+                className={'input'}
+                type="text"
+                placeholder={'Почта'}
+                value={mail}
+                onChange={onChangemail}
+
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
