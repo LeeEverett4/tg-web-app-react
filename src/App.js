bc49ba13ad20142f0ea97ccd5d658b3282f0a360
leeeverett4 <kazakoff.iurij2018@gmail.com>
2023-05-18 20:36:44 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/Productlist/Productlist";
 import Form from "./components/Form/Form";
+import Form2 from "./components/Form2/Form2";
 function App() {
     const {onToggleButton, tg} = useTelegram();
   useEffect(() =>
@@ -19,6 +20,7 @@ function App() {
         <Routes>
             <Route index element={<ProductList />}/>
             <Route path={'form'} element={<Form />}/>
+            <Route path={'form2'} element={<Form2 />}/>
         </Routes>
 
 
